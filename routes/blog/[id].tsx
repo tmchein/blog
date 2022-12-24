2ae058442dc141e25b10ffb9d3e20a0bf6003ea6
Juan Rojas <davidr2998@gmail.com>
2022-12-24 11:55:17 -0500

    added back button

--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -2,6 +2,7 @@ import { Handlers, PageProps } from "$fresh/server.ts";
 import { CSS } from "https://deno.land/x/gfm@0.1.26/mod.ts";
 import { Layout } from "../../components/Layout.tsx";
 import { loadPost } from "../../utils/posts.ts";
+import { asset } from "$fresh/runtime.ts";
 
 export const handler: Handlers = {
   async GET(request, context) {
@@ -16,14 +17,23 @@ export default function PagePost(props: PageProps) {
   return (
     <Layout title={post.title}>
       <article>
-        <header class="text-center bg-blue-500 px-4 py-2 text-white">
-          <h1 class="text-3xl font-bold">
-            {post.title}
-          </h1>
-          <time>
-            <strong>Publicado:</strong>{" "}
-            {Intl.DateTimeFormat("es").format(post.date)}
-          </time>
+        <header class="relative text-center bg-blue-500 px-16 md:px-4 py-2 text-white">
+          <a class="absolute m-auto left-5 top-[calc(50%-16px)]" href="/">
+            <img
+              class="w-8 h-8"
+              src={asset("/back.svg")}
+              alt="back to main page"
+            />
+          </a>
+          <section class="col-start-2 col-end-4">
+            <h1 class="text-3xl font-bold">
+              {post.title}
+            </h1>
+            <time>
+              <strong>Publicado:</strong>{" "}
+              {Intl.DateTimeFormat("es").format(post.date)}
+            </time>
+          </section>
         </header>
         <main class="pt-8 px-6 md:px-32 xl:px-[400px]">
           <style dangerouslySetInnerHTML={{ __html: CSS }} />
