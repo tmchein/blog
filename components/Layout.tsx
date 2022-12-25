300fb94b58ea258d2f26262aff4c17803f7977d4
Juan Rojas <davidr2998@gmail.com>
2022-12-24 20:14:18 -0500

    add: adding OG tags to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,6 +8,29 @@ export function Layout(
     <div class="min-h-screen bg-[#22272E] flex flex-col">
       <head>
         <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="blog" />
+        <meta property="og:locale" content="es-CO" />
+        <meta
+          property="og:url"
+          content="https://www.tmblog.deno.dev"
+        />
+        <meta
+          property="og:image"
+          content="https://i.postimg.cc/vmRNk2gk/tmblog-og-image.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="720" />
+        <meta
+          property="og:image:alt"
+          content="A gray background with a TM logo in the center."
+        />
+        <meta property="og:site_name" content="TMBlog" />
+        <meta
+          property="og:description"
+          content="Blog de opinión personal, tecnología, programación y desarrollo web."
+        />
       </head>
       <main class="flex-grow-1">
         {children}
